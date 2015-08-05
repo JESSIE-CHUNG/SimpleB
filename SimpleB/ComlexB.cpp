51e0f79f4b854ef18b426e2570e575cb79b70c76
Jessie Chung <ektmf1921@gmail.com>
2015-08-05 20:30:18 +0900

    My 3rd commit

---
+++ SimpleB/ComlexB.cpp
@@ -0,0 +1,19 @@
+// SimpleB.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello, Git!" << "\n";
+	cout << "Staging modified files" << "\n";
+	
+	system("PAUSE"); 
+		return 0;
+
+}
+
