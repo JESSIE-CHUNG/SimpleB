902292331446b2a54f029f160d012e1aac8afa91
Jessie Chung <ektmf1921@gmail.com>
2015-08-05 14:59:32 +0900

    my first commit

---
+++ SimpleB/SimpleB.cpp
@@ -0,0 +1,15 @@
+// SimpleB.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello, Git!" << "\n";
+
+	return 0;
+}
+
