46ce905fb904d0c6498d739a58e7442c34834d8e
Jessie Chung <ektmf1921@gmail.com>
2015-08-05 16:28:51 +0900

    My second commit

--- SimpleB/SimpleB.cpp
+++ SimpleB/SimpleB.cpp
@@ -3,13 +3,17 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Hello, Git!" << "\n";
+	cout << "Staging modified files" << "\n";
+	
+	system("PAUSE"); 
+		return 0;
 
-	return 0;
 }
 
